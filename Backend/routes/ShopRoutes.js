8ba5d1a8cb12cc4ce9cc71a1e5846366604a4fa7
Gabriel-developments <salvadiploma5@gmail.com>
2026-04-20 10:08:11 -0300

    Initial implementation of middleware with JWT and route authentication.

--- Backend/routes/ShopRoutes.js
+++ Backend/routes/ShopRoutes.js
@@ -1,19 +1,20 @@
 import * as ShopController from '../controllers/ShopController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 import express from 'express';
 const ShopRoutes = express.Router();
 
 
-ShopRoutes.post('/shops', ShopController.createShop);
-ShopRoutes.get('/shops', ShopController.getShops);
-ShopRoutes.get('/shops/:id', ShopController.getShopById);
-ShopRoutes.put('/shops/:id', ShopController.updateShop);
-ShopRoutes.delete('/shops/:id', ShopController.deleteShop);
+ShopRoutes.post('/shops', authMiddleware, ShopController.createShop);
+ShopRoutes.get('/shops', authMiddleware, ShopController.getShops);
+ShopRoutes.get('/shops/:id', authMiddleware, ShopController.getShopById);
+ShopRoutes.put('/shops/:id', authMiddleware, ShopController.updateShop);
+ShopRoutes.delete('/shops/:id', authMiddleware, ShopController.deleteShop);
 
-ShopRoutes.post('/products', ShopController.createProduct);
-ShopRoutes.get('/products', ShopController.getProducts);
-ShopRoutes.get('/products/:id', ShopController.getProductById);
-ShopRoutes.put('/products/:id', ShopController.updateProduct);
-ShopRoutes.delete('/products/:id', ShopController.deleteProduct);
+ShopRoutes.post('/products', authMiddleware, ShopController.createProduct);
+ShopRoutes.get('/products', authMiddleware, ShopController.getProducts);
+ShopRoutes.get('/products/:id', authMiddleware, ShopController.getProductById);
+ShopRoutes.put('/products/:id', authMiddleware, ShopController.updateProduct);
+ShopRoutes.delete('/products/:id', authMiddleware, ShopController.deleteProduct);
 
 
 export default ShopRoutes;
